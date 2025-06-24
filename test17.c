07a05b9ef7aa2f066cb7d3f192aad995fdcd0288
Dafetite <ogagadafetite@gmail.com>
2025-06-24 01:52:11 +0100

    Create test17.c

---
+++ test17.c
@@ -0,0 +1,6 @@
+print_norm(stashedOutput)
+		stashCreated = Trueprint_norm(stashedOutput)
+		stashCreated = Trueprint_norm(stashedOutput)
+		stashCreated = Trueprint_norm(stashedOutput)
+		stashCreated = Trueprint_norm(stashedOutput)
+		stashCreated = True
